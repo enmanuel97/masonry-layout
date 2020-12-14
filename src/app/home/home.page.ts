1e41c282be8b84c56384fc019f7e7c3517a40aaa
Jesus Enmanuel <edelacruz9713@gmail.com>
2020-12-13 20:01:04 -0400

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  animales: any[] = [
+    {
+      img: 'img1.jpg'
+    },
+    {
+      img: 'img2.jpg'
+    },
+    {
+      img: 'img3.jpg'
+    },
+    {
+      img: 'img4.jpg'
+    },
+    {
+      img: 'img5.jpg'
+    },
+    {
+      img: 'img6.jpg'
+    },
+    {
+      img: 'img7.jpg'
+    },
+    {
+      img: 'img8.jpg'
+    },
+    {
+      img: 'img9.jpg'
+    },
+  ];
   constructor() {}
 
 }
